9297eab7d3ba618731ec3e083e8f5793f63b5321
Grvy <gerwy@mailbox.org>
2022-02-24 11:50:15 +0100

    globalnie

--- Sudoku/Sudoku.cpp
+++ Sudoku/Sudoku.cpp
@@ -2,23 +2,20 @@
 
 using namespace std;
 
-void draw_board(int _board[9][9]) {
+int board[9][9] { 0 };
 
+void draw_board() {
     for (int i = 0; i < 9; i++, cout << "\n") {
         for (int j = 0; j < 9; j++) {
             if (j % 3 == 0 || j == 0) cout << "| ";
-            cout << _board[i][j] << " ";
+            cout << board[i][j] << " ";
         }
     }
 
 }
 
 int main() {
-
-    int board[9][9] { 0 };
-
     draw_board(board);
-
 }
 
 
