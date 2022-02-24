734bc325d16cb3b1915676a8039ab73371a3297c
Kl4y4 <surowiec.bartosz@wp.pl>
2022-02-24 11:58:17 +0100

    draw_board do sprawdzenia yo

--- Sudoku/Sudoku.cpp
+++ Sudoku/Sudoku.cpp
@@ -1,23 +1,28 @@
 ﻿#include <iostream>
 
 using namespace std;
+int board[9][9]{ 0 };
 
-void draw_board(int _board[9][9]) {
+
+
+void draw_board() {
 
     for (int i = 0; i < 9; i++, cout << "\n") {
+        if (i % 3 == 0) cout << "-------------------------\n";
         for (int j = 0; j < 9; j++) {
-            if (j % 3 == 0 || j == 0) cout << "| ";
-            cout << _board[i][j] << " ";
+            if (j % 3 == 0) cout << "| ";
+            cout << board[i][j] << " ";
+            if (j == 8) cout << "| ";
         }
+        if (i == 8) cout << "\n-------------------------\n";
     }
 
 }
 
 int main() {
 
-    int board[9][9] { 0 };
 
-    draw_board(board);
+    draw_board();
 
 }
 
