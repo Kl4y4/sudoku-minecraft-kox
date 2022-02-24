1b2f5e8c48e5859aa740e4df9ebde4ea09eb6654
Grvy <gerwy@mailbox.org>
2022-02-24 12:25:15 +0100

    sprawdzanko

--- Sudoku/Sudoku.cpp
+++ Sudoku/Sudoku.cpp
@@ -4,6 +4,20 @@ using namespace std;
 
 int board[9][9] { 0 };
 
+int chunk_centers[9][2] = {
+    {0,0},
+    {3,0},
+    {6,0},
+
+    {0,3},
+    {3,3},
+    {6,3},
+
+    {0,6},
+    {3,6},
+    {6,6},
+};
+
 void draw_board() {
     for (int i = 0; i < 9; i++, cout << "\n") {
         for (int j = 0; j < 9; j++) {
@@ -11,11 +25,32 @@ void draw_board() {
             cout << board[i][j] << " ";
         }
     }
+}
 
+bool is_valid(int x, int y) {
+    int l = board[y][x];
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            if (i == y && j != x) if (board[i][j] == l) return false;  // |
+            if (j == x && i != y) if (board[i][j] == l) return false ; // -
+        }
+    }
+  
+    int chunk_x = chunk_centers[(x / 3) + ((y / 3) * 3)][0];
+    int chunk_y = chunk_centers[(x / 3) + ((y / 3) * 3)][1];
+    
+    int o = 0;
+    for (int i = chunk_y; i < chunk_y + 3; i++) {
+        for (int j = chunk_x; j < chunk_x + 3; j++) {
+            o += board[i][j] == l;
+        }
+    }
+    if (o >= 2) return false;
+    else return true;
 }
 
 int main() {
-    draw_board(board);
+    draw_board();
 }
 
 
