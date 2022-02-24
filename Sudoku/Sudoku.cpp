85e8f6597d4659f1ed51e8dd4fc8b5cd121005ed
g3rwy <gerwy@mailbox.org>
2022-02-24 21:32:15 +0100

    A no sprawdziłem sprawdzanie i jakaś tam zmiana

--- Sudoku/Sudoku.cpp
+++ Sudoku/Sudoku.cpp
@@ -1,10 +1,13 @@
 ﻿#include <iostream>
+#include <random>
+#include <string>
+#include <stdio.h>
 
 using namespace std;
 
 int board[9][9] { 0 };
 
-int chunk_centers[9][2] = {
+const int chunk_centers[9][2] = {
     {0,0},
     {3,0},
     {6,0},
@@ -19,7 +22,14 @@ int chunk_centers[9][2] = {
 };
 
 void draw_board() {
+    // dodałem se indexy żeby łatwiej się sprawdzało, można dodać potem czy coś
+    // printf("    ");
+    // for (int i = 0; i < 9; i++) cout << (i % 3 == 0 && i != 0 ? "   " : " ") << i;
+    // putc('\n',stdout); 
+    //printf("    ");
+    //cout << string(9 * 3 - 4,'-') << "\n";
     for (int i = 0; i < 9; i++, cout << "\n") {
+        // cout << i << "  ";
         for (int j = 0; j < 9; j++) {
             if (j % 3 == 0 || j == 0) cout << "| ";
             cout << board[i][j] << " ";
@@ -28,7 +38,10 @@ void draw_board() {
 }
 
 bool is_valid(int x, int y) {
-    int l = board[y][x];
+    int l = board[y][x];    
+    if(l == 0) // check
+        cout << "yo ziom pomyłka, sprawdzasz puste pole, wstaw tam coś najpierw\n"; return false;
+    
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
             if (i == y && j != x) if (board[i][j] == l) return false;  // |
@@ -45,14 +58,10 @@ bool is_valid(int x, int y) {
             o += board[i][j] == l;
         }
     }
-    if (o >= 2) return false;
-    else return true;
+    return (o == 1);
 }
 
 int main() {
     draw_board();
 }
-
-
-
 
