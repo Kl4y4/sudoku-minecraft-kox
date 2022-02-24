854f2095a65eee1d27b721af2ed2fc1a92926851
Kl4y4 <surowiec.bartosz@wp.pl>
2022-02-24 11:32:26 +0100

    draw_board v1

--- Sudoku/Sudoku.cpp
+++ Sudoku/Sudoku.cpp
@@ -2,9 +2,25 @@
 
 using namespace std;
 
+void draw_board(int _board[9][9]) {
+
+    for (int i = 0; i < 9; i++, cout << "\n") {
+        for (int j = 0; j < 9; j++) {
+            if (j % 3 == 0 || j == 0) cout << "| ";
+            cout << _board[i][j] << " ";
+        }
+    }
+
+}
+
 int main() {
 
+    int board[9][9] { 0 };
 
+    draw_board(board);
 
-    std::cout << "Hello World!\n";
 }
+
+
+
+
