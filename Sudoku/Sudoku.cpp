be5c991d65dad36296450871862045f758b730fe
Kl4y4 <surowiec.bartosz@wp.pl>
2022-03-01 10:02:05 +0100

    Merge do maina

--- Sudoku/Sudoku.cpp
+++ Sudoku/Sudoku.cpp
@@ -1,9 +1,15 @@
 ﻿#include <iostream>
 #include <string>
+//#include "kolorki.hpp"
+#include <random>
+
+#define MAX_NUMBERS 10
+
 
 using namespace std;
 
 int board[9][9] { 0 };
+int wylosowane[MAX_NUMBERS][2]{ 0 };
 
 int chunk_centers[9][2] = {
     {0,0},
@@ -56,25 +62,44 @@ void get_user_input(int& x, int& y) {
 
 bool is_valid(int x, int y) {
     int l = board[y][x];
+    //if(l == 0) // check
+
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
             if (i == y && j != x) if (board[i][j] == l) return false;  // |
-            if (j == x && i != y) if (board[i][j] == l) return false ; // -
+            if (j == x && i != y) if (board[i][j] == l) return false; // -
         }
     }
-  
+
     int chunk_x = chunk_centers[(x / 3) + ((y / 3) * 3)][0];
     int chunk_y = chunk_centers[(x / 3) + ((y / 3) * 3)][1];
-    
+
     int o = 0;
     for (int i = chunk_y; i < chunk_y + 3; i++) {
         for (int j = chunk_x; j < chunk_x + 3; j++) {
             o += board[i][j] == l;
         }
     }
-    if (o >= 2) return false;
-    else return true;
+    return (o == 1);
 }
+
+void shuffle_numbers(int count) {
+    int idx = 0;
+    while (count) {
+        int x = rand() % 9, y = rand() % 9;
+        int n = rand() % 9 + 1;
+        if (board[y][x] != 0) continue;
+        board[y][x] = n;
+        if (is_valid(x, y)) {
+            count--;
+            wylosowane[idx][0] = x;
+            wylosowane[idx++][1] = y;
+        }
+        else board[y][x] = 0;
+    }
+}
+
+
 
 int main() {
 
