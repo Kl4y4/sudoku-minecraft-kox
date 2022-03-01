87318727e4d45346231d4333c47a9b9ce6969ff3
Grvy <gerwy@mailbox.org>
2022-03-01 09:37:24 +0100

    Losowanie liczb

--- Sudoku/Sudoku.cpp
+++ Sudoku/Sudoku.cpp
@@ -1,9 +1,14 @@
 ﻿#include <iostream>
 #include "kolorki.hpp"
+#include <random>
+
+#define MAX_NUMBERS 10
 
 using namespace std;
 
 int board[9][9] { 0 };
+
+int wylosowane[MAX_NUMBERS][2]{ 0 };
 
 const int chunk_centers[9][2] = {
     {0,0},
@@ -20,14 +25,7 @@ const int chunk_centers[9][2] = {
 };
 
 void draw_board() {
-    // dodałem se indexy żeby łatwiej się sprawdzało, można dodać potem czy coś
-    // printf("    ");
-    // for (int i = 0; i < 9; i++) cout << (i % 3 == 0 && i != 0 ? "   " : " ") << i;
-    // putchar('\n'); 
-    //printf("    ");
-    //cout << string(9 * 3 - 4,'-') << "\n";
     for (int i = 0; i < 9; i++, cout << "\n") {
-        // cout << i << "  ";
         for (int j = 0; j < 9; j++) {
             if (j % 3 == 0 || j == 0) cout << "| ";
             cout << board[i][j] << " ";
@@ -35,10 +33,10 @@ void draw_board() {
     }
 }
 
+
 bool is_valid(int x, int y) {
     int l = board[y][x];    
-    if(l == 0) // check
-        cout << "yo ziom pomyłka, sprawdzasz puste pole, wstaw tam coś najpierw\n"; return false;
+    //if(l == 0) // check
     
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
@@ -59,6 +57,26 @@ bool is_valid(int x, int y) {
     return (o == 1);
 }
 
+void shuffle_numbers(int count) {
+    int idx = 0;
+    while (count) {
+        int x = rand() % 9, y = rand() % 9;
+        int n = rand() % 9 + 1;
+        if (board[y][x] != 0) continue;
+        board[y][x] = n;
+        if (is_valid(x, y)) {
+            count--;
+            wylosowane[idx][0] = x;
+            wylosowane[idx++][1] = y;
+        }
+        else board[y][x] = 0;
+    }
+}
+
 int main() {
+    srand(time(0));
+    draw_board();
+    puts("\n--------------------------------------------");
+    shuffle_numbers(10);
     draw_board();
 }
