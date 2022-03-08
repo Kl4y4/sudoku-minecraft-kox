0b4d61ae41057eb364268df6302dd43c7d5e1259
Kl4y4 <surowiec.bartosz@wp.pl>
2022-03-08 15:38:59 +0100

    Brakuje AI

--- Sudoku/Sudoku.cpp
+++ Sudoku/Sudoku.cpp
@@ -1,17 +1,16 @@
 ﻿#include <iostream>
-#include <string>
-//#include "kolorki.hpp"
+#include "kolorki.hpp"
 #include <random>
 
 #define MAX_NUMBERS 10
 
-
 using namespace std;
 
-int board[9][9] { 0 };
+int board[9][9]{ 0 };
+
 int wylosowane[MAX_NUMBERS][2]{ 0 };
 
-int chunk_centers[9][2] = {
+const int chunk_centers[9][2] = {
     {0,0},
     {3,0},
     {6,0},
@@ -26,38 +25,18 @@ int chunk_centers[9][2] = {
 };
 
 void draw_board() {
+    cout << "   1 2 3   4 5 6   7 8 9\n";
     for (int i = 0; i < 9; i++, cout << "\n") {
-        if (i % 3 == 0) cout << "-------------------------\n";
+        if (i % 3 == 0) writeInStyle(CYAN, "  ------------------------\n", 0);
         for (int j = 0; j < 9; j++) {
-            if (j % 3 == 0) cout << "| ";
+            if (j == 0) cout << i + 1;
+            if (j % 3 == 0) writeInStyle(CYAN, "| ", 0);
             cout << board[i][j] << " ";
-            if (j == 8) cout << "| ";
+            if (j == 8) writeInStyle(CYAN, "| ", 0);
         }
-        if (i == 8) cout << "\n-------------------------\n";
+        if (i == 8) writeInStyle(CYAN, "\n  ------------------------\n", 0);
     }
 }
-
-bool check_for_round_end() {
-    for (int i = 0; i < 9; i++)
-        for (int j = 0; j < 9; j++)
-            if (board[i][j] == 0) return false;
-    return true;
-}
-
-void get_user_input(int& x, int& y) {
-
-    puts("WYKONAJ RUCH\n");
-    cin >> x >> y;
-    
-    for (int i = 0; i < 10; i++) {
-        if (y == wylosowane[i][0] && x == wylosowane[i][1]) {
-            puts("Wybrane pole jest już zajęte!!!!1!!1!");
-            get_user_input(x, y);
-        }
-    }
-
-}
-
 
 
 bool is_valid(int x, int y) {
@@ -83,6 +62,33 @@ bool is_valid(int x, int y) {
     return (o == 1);
 }
 
+bool check_for_round_end() {
+    for (int i = 0; i < 9; i++)
+        for (int j = 0; j < 9; j++)
+            if (board[i][j] == 0) return false;
+    return true;
+}
+
+void get_user_input(int& x, int& y, int& number) {
+
+    puts("WYKONAJ RUCH\n");
+    cin >> x >> y >> number;
+    x--; y--;
+
+    if (number < 1 || number > 9 || x < 0 || x > 8 || y < 0 || y > 8) {
+        puts("Zle numerki wybrany6 kolego");
+        get_user_input(x, y, number);
+    }
+
+    for (int i = 0; i < MAX_NUMBERS; i++) {
+        if (y == wylosowane[i][0] && x == wylosowane[i][1] || !is_valid(x, y)) {
+            puts("Wybrane pole jest ZLE!!!!1!!1!");
+            get_user_input(x, y, number);
+        }
+    }
+
+}
+
 void shuffle_numbers(int count) {
     int idx = 0;
     while (count) {
@@ -99,15 +105,13 @@ void shuffle_numbers(int count) {
     }
 }
 
-
-
 int main() {
 
-    int x, y;
-    get_user_input(x, y);
-    printf("X: %i; Y: %i", x, y);
+    // input gracza gamera
+    int x, y, number;
 
-    draw_board();
+    char b = '0xE2';
+    cout << b;
+
+
 }
-
-
---
+++ Sudoku/kolorki.hpp
@@ -0,0 +1,59 @@
+#include <cstdio>
+#include <string>
+#include <iostream>
+
+/*
+#define BLACK_PAWN "♙"
+#define BLACK_KNIGHT "♘"
+#define BLACK_BISHOP "♗"
+#define BLACK_ROOK "♖"
+#define BLACK_QUEEN "♕"
+#define BLACK_KING "♔"
+
+#define WHITE_PAWN "♟︎"
+#define WHITE_KNIGHT "♞"
+#define WHITE_BISHOP "♝"
+#define WHITE_ROOK "♜"
+#define WHITE_QUEEN "♛"
+#define WHITE_KING "♚"
+*/
+
+// https://wikiless.alefvanoon.xyz/wiki/ANSI_escape_code?lang=en#SGR_(Select_Graphic_Rendition)_parameters
+// Style do wybrania
+enum STYLES {
+    RESET = 0,
+    INVERSE = 7,   // Zamienia miejscami kolor tła i tekstu
+    BLINK = 5,     // Mryga lol
+    UNDERLINE = 4, // Podkreśla
+    BLACK = 30, // kolor tekstu
+    RED,
+    GREEN,
+    BLUE = 34,
+    VIOLET,
+    CYAN,
+    B_BLACK = 40, // kolor tła
+    B_RED,
+    B_GREEN,
+    B_BLUE = 44,
+    B_VIOLET,
+    B_CYAN,
+    B_WHITE
+};
+
+//std::wstring black_pieces[] = { L"♘",L"♗",L"♖",L"♕",L"♔" };
+//std::wstring white_pieces[] = { L"♟︎",L"♞",L"♝",L"♜",L"♛",L"♚" };
+
+// Mozna uzyć zeby zmienic styl dla wszystkiego co bedzie napisane nastepne
+void changeStyle(const int style) {
+    printf("\033[%dm", style);
+}
+
+// Napisze podany tekst w podanym stylu
+void writeInStyle(const int style, const std::string text, const int padding = 0) {
+    changeStyle(RESET);
+    for (int i = 0; i < padding; ++i) putc(' ', stdout); // lewa strona
+    changeStyle(style);
+    std::cout << text.c_str();
+    changeStyle(RESET);
+    for (int i = 0; i < padding; ++i) putc(' ', stdout); // prawa strona
+}
